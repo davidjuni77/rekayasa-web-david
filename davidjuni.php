51a372c024bea106b4b5cae8841a7bcbd3aac2be
davidjuni77 <136463896+davidjuni77@users.noreply.github.com>
2024-10-08 21:40:04 +0700

    Add files via upload

---
+++ davidjuni.php
@@ -0,0 +1,21 @@
+<?php
+// Membuat variabel JSON
+$jsonData = '{"nama":"Maulana Malik Ibrahim","usia":21,"pekerjaan":"IT Education","hobi":["memancing","futsal","joging"]}';
+
+// Decode JSON ke Objek
+$jsonObject = json_decode($jsonData);
+
+// Mengakses Objek JSON
+echo "Mengakses nilai dari PHP Objek:" . "<br>";
+echo "Nama: " . $jsonObject->nama . "<br>";
+echo "Usia: " . $jsonObject->usia . "<br>";
+echo "Pekerjaan: " . $jsonObject->pekerjaan . "<br><br>";
+
+// Mengakses Array Hobi
+echo "Mengakses Nilai dari PHP Array:" . "<br>";
+echo "Hobi:" . "<br>";
+echo "<ul>";
+foreach ($jsonObject->hobi as $key => $hobi) {
+    echo "<li>". $hobi ."</li>";
+};
+echo "</ul>";
